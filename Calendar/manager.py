8ab7e2ef6d6950a57ae716694da5da7d7a6432a2
Earoisn <invisibleconversation@gmail.com>
2023-05-02 12:37:27 -0300

    sacar import duplicado

--- Calendar/manager.py
+++ Calendar/manager.py
@@ -4,7 +4,7 @@ import json
 import sys
 sys.path.extend(["D:\\code\\gcloud","D:\\code\\gcloud\\Calendar"])
 from Google import glogin, get_service
-from event import Event
+
 
 días = {0: "lunes", 1: "martes", 2: "miércoles", 3: "jueves", 4: "viernes", 5: "sábado", 6: "domingo"}
 
